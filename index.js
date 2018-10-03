810b75604dfbada034ac63c64b40c779c211a82d
Nate Miller <natekmiller96@gmail.com>
2018-10-03 04:59:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ class Route{
   
   blocksTravelled(){
   let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
-  let vertDistance = 
+  let vertDistance = this.endingLocation.vertical - this.startingLocation.vertical;
+  
   }
 }
