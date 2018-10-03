7e1bb006091ab43dd9b402a2eca02861e7b55617
Nate Miller <natekmiller96@gmail.com>
2018-10-03 04:45:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class Driver{
   }
  
   yearsExperienceFromBeginningOf(date) {
-    return this.startDate.getFullYear() - date;
+    return date - this.startDate.getFullYear();
   }
   approve() {
     return 'You can do that!';
