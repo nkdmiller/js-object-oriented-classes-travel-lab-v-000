55c60b63454057203c326a87c623b7cfdde72ac6
Nate Miller <natekmiller96@gmail.com>
2018-10-03 05:14:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
+class Driver{
+  constructor(name, startDate) {
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+ 
+  yearsExperienceFromBeginningOf(date) {
+    return date - this.startDate.getFullYear();
+  }
 
+}
+ 
+ 
+class Route{
+  constructor(beginningLocation, endingLocation) {
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  
+  blocksTravelled(){
+  let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+  let vertDistance = this.endingLocation.vertical - this.beginningLocation.vertical;
+  let horizDistance = eastWest.indexOf(this.endingLocation.horizontal) - eastWest.indexOf(this.beginningLocation.horizontal);
+  return vertDistance + horizDistance;
+  }
+  
+  estimatedTime(peakTime){
+    if (peakTime === true){
+      return this.blocksTravelled() / 2;
+    }
+    else {
+      return this.blocksTravelled() / 3;  
+    }
+  }
+}
